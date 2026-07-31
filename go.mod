4dc9bfbfc3220af7662545685a915c8761c32e21
smassely <smassollol@gmail.com>
2026-07-31 14:38:06 +0100

    updates

--- go.mod
+++ go.mod
@@ -2,10 +2,11 @@ module gridstuff
 
 go 1.26.1
 
+require github.com/gen2brain/raylib-go/raylib v0.60.0
+
 require (
-	github.com/ebitengine/purego v0.10.0 // indirect
-	github.com/gen2brain/raylib-go/raygui v0.0.0-20260309161816-4c350487f71d // indirect
-	github.com/gen2brain/raylib-go/raylib v0.55.1 // indirect
-	golang.org/x/exp v0.0.0-20260312153236-7ab1446f8b90 // indirect
-	golang.org/x/sys v0.42.0 // indirect
+	github.com/ebitengine/purego v0.10.2 // indirect
+	github.com/jupiterrider/ffi v0.7.0 // indirect
+	golang.org/x/exp v0.0.0-20260727155853-b88d891fe743 // indirect
+	golang.org/x/sys v0.47.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	//	"os"
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
@@ -71,27 +73,64 @@ func getCardAbsPos(c *card) *pos {
 	return newPos((c.grid.grid_start.x + c.grid.block_size*c.grid_pos.x), (c.grid.grid_start.y + c.grid.block_size*c.grid_pos.y))
 }
 
-func checkCardInteraction(c *card) bool {
+func checkCardInteraction(c *card, mousepos rl.Vector2) bool {
 	rec := rl.Rectangle{float32(getCardAbsPos(c).x), float32(getCardAbsPos(c).y), float32(c.grid.block_size), float32(c.grid.block_size)}
-	if rl.CheckCollisionPointRec(rl.GetMousePosition(), rec) {
+	if rl.CheckCollisionPointRec(mousepos, rec) {
 		return true
 	} else {
 		return false
 	}
+}
+func isEdgeCard(c card) bool {
+	g := c.grid
+	if c.grid_pos.x == 0 || c.grid_pos.y == 0  || c.grid_pos.x == g.width-1 || c.grid_pos.y == g.height-1{
+		return true
+	}
+	return false
+}
+func getGridCorners(g grid) []pos {
+	corners := make([]pos, 4)
+	corners[0] = pos{0,0}
+	corners[1] = pos{g.width-1,0}
+	corners[2] = pos{0,g.height-1}
+	corners[3] = pos{g.width-1,g.height-1}
+	return corners
+}
+
+func isCornerCard(c card) bool{
+	corners := getGridCorners(c.grid)
+	for corner := 0; corner<4; corner++{
+		if c.grid_pos == corners[corner] {
+			return true
+		}
+	}
+	return false
 }
 
 func main() {
 	// == Declarations ==
 	gameGrid := newGrid(8, 8, 100, *newPos(10, 10))
 	cards := make([]card, 0)
+	fmt.Println(gameGrid.width, gameGrid.height)
 
 	windowWidth := (gameGrid.width)*gameGrid.block_size + gameGrid.grid_start.x*2
 	windowHeight := (gameGrid.height)*gameGrid.block_size + gameGrid.grid_start.y*2
 	tool := pen
+	mousepos := rl.GetMousePosition()
+
+	
 	// ==Game Event Loop==
 
+	//os.Unsetenv("WAYLAND_DISPLAY")
 	rl.InitWindow(windowWidth, windowHeight, "gridstuff")
 	defer rl.CloseWindow()
+	// ==Texture Loading ==
+
+	img := rl.LoadImage("./texture.png")
+	rl.ImageResize(img, gameGrid.block_size, gameGrid.block_size)
+	texture := rl.LoadTextureFromImage(img)
+	rl.UnloadImage(img)
+
 	rl.SetTargetFPS(100)
 	for cardx := 0; cardx < int(gameGrid.width); cardx++ {
 		for cardy := 0; cardy < int(gameGrid.height); cardy++ {
@@ -100,9 +139,18 @@ func main() {
 	}
 
 	for !rl.WindowShouldClose() {
+		windowWidth = (gameGrid.width)*gameGrid.block_size + gameGrid.grid_start.x*2
+		windowHeight = (gameGrid.height)*gameGrid.block_size + gameGrid.grid_start.y*2
+		rl.SetWindowSize(int(windowWidth), int(windowHeight))
+		for cardx := 0; cardx < int(gameGrid.width); cardx++ {
+			for cardy := 0; cardy < int(gameGrid.height); cardy++ {
+				cards = append(cards, *newCard(*gameGrid, *newPos(int32(cardx), int32(cardy))))
+			}
+		}
+		mousepos = rl.GetMousePosition()
 		for card := 0; card < len(cards); card++ {
 			c := &cards[card]
-			if checkCardInteraction(c) {
+			if checkCardInteraction(c,mousepos) {
 				c.hover = true
 				if rl.IsMouseButtonDown(rl.MouseButtonLeft) {
 					switch tool {
@@ -116,25 +164,44 @@ func main() {
 				c.hover = false
 			}
 		}
-
+		
 		if rl.IsKeyPressed(rl.KeyA) {
 			tool++
 			tool = tool % max
 		}
+		if rl.IsKeyPressed(rl.KeyRight){
+				gameGrid.width++
+				cards = make([]card, 0)
+		} else if rl.IsKeyPressed(rl.KeyLeft){
+			gameGrid.width--
+			cards = make([]card, 0)
+		}
+		if rl.IsKeyPressed(rl.KeyUp){
+				gameGrid.height++
+				cards = make([]card, 0)
+		} else if rl.IsKeyPressed(rl.KeyDown){
+			gameGrid.height--
+			cards = make([]card, 0)
+		}
 
-		// ==Game Draw Loop ==
+		// ==Game Draw Loop == \\
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.RayWhite)
 
 		for card := 0; card < len(cards); card++ {
 			c := cards[card]
 			rec := rl.NewRectangle(float32(getCardAbsPos(&c).x+(c.grid.block_size/10)*0), float32(getCardAbsPos(&c).y+int32(c.grid.block_size/10)*0), float32(c.grid.block_size), float32(c.grid.block_size))
-
-			rl.DrawRectangleRec(rec, c.color)
+			//rl.DrawTexture(texture, getCardAbsPos(&c).x,getCardAbsPos(&c).y, rl.White)
+			rl.DrawRectangleRec(rec, rl.Blue)
+			if isEdgeCard(c) {rl.DrawRectangleRec(rec, rl.Red)}
+			if isCornerCard(c) {rl.DrawRectangleRec(rec, rl.Black)}
+			//rl.DrawRectangleRec(rec, c.color)
 			if c.hover {
 				rl.DrawRectangleLinesEx(rec, 2, rl.White)
 			}
 		}
 		rl.EndDrawing()
+		
 	}
+	rl.UnloadTexture(texture)
 }
